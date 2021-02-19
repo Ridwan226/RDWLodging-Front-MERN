2dc0e92d95c7c8c3ce3919980f9e4ef4383606b7
Ridwan226 <4515210063@univpancasila.ac.id>
2021-02-20 00:15:14 +0700

    Most Picked Selesai

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import Header from "./parts/Header";
 import landingPage from "../json/landingPage.json";
 import Hero from "./parts/Hero";
+import MostPicked from "./parts/MostPicked";
 
 export default class LandingPages extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+
   render() {
     return (
       <div>
         <Header {...this.props} />
-        <Hero data={landingPage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </div>
     );
   }
